c88fcd9f8b0f52a81abdb4e9ec0fa2c193f3bbf5
djanpedra113-svg <djanpedra113@gmail.com>
2026-05-28 15:19:50 +0100

    Prepare backend for deployment

--- backend FORIO/server.js
+++ backend FORIO/server.js
@@ -113,6 +113,7 @@ app.get("/admin/results", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log(`Server running on ${PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
